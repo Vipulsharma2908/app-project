bf896c742ac65adfdfb364227d5d2b233d88fbe9
vipulsharma2908 <vipul.sharma2908@gmail.com>
2023-02-27 13:27:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{display: 'flex', alignContent:'center', justifyContent: 'center'}}>
+      Welcome to the React E-commerce App!!
+    </div>
+  );
+}
+
+export default App;
